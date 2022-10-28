54d4a2c0fbdf43ed212041ecc7a7f6b8eea7acc1
jaemin <anjm1020@gmail.com>
2022-10-28 20:15:55 +0900

    Feat : add init.js

---
+++ module/init.js
@@ -0,0 +1,11 @@
+const Station = require("../model/station");
+const openData = require("../module/openData");
+
+let dataRows = openData.getStationCostInfoDataFromCsv();
+for (let i = 0; i < dataRows.length; i++) {
+    let data = dataRows[i];
+    Station.insert(data).then(() => console.log(data + '\n'))
+        .catch(e => console.error(e));
+}
+
+console.log("======INIT DATABASE======");
