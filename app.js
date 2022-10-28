eac4050a5ff9a22e1d9eae247f332b9da24f727a
jaemin <anjm1020@gmail.com>
2022-10-28 20:01:18 +0900

    Fix : typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const schedule = require("node-schedule");
+const cron = require("cron");
 
 const batch = require("./module/batch");
 const apiRouter = require('./routes/api');
@@ -19,9 +19,9 @@ app.use('/api', apiRouter);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log("server start");
-    schedule.scheduledJobs('0 0 0 * * *', ()=>{
+    cron.job('0 0 0 * * *', () => {
         batch()
             .then(() => console.log("Batch jobs complete\n"))
             .catch(e => console.error(e));
-    });
+    }).start();
 });
